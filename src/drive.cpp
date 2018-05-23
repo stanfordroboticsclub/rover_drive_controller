974ced7d91fd845902a0b016db9b0a09acffd9ce
Chandler Watson <watsonc@stanford.edu>
2018-05-23 02:13:40 -0700

    Finish driver code (heh, sure, "finish" he says at 11:05pm, sure)

--- src/drive.cpp
+++ src/drive.cpp
@@ -3,15 +3,31 @@
 #include "geometry_msgs/Twist.h"
 
 ros::Publisher drive_pub;
+ros::Publisher dig_pub;
+ros::Publisher halt_pub_front_left;
+ros::Publisher halt_pub_back_left;
+ros::Publisher halt_pub_back_right;
+
 bool digging_state = false;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg) {
-    digging_state = joy_msg->buttons[4];
+    bool new_digging_state = joy_msg->buttons[4];
+    if (!digging_state && new_digging_state) {
+        std::cout << "...and now we're digging!" << std::endl;
+    } else if (digging_state && !new_digging_state) {
+        std::cout << "...and now we're driving, you absolute scrub." << std::endl;
+    }
+    digging_state = new_digging_state;
 }
 
 void twistCallback(const geometry_msgs::Twist::ConstPtr& cmd_vel) {
     if (digging_state) {
-        // pass
+        geometry_msgs::Twist dig_vel, halt_vel;
+        dig_vel.linear.y = cmd_vel->linear.y;
+        dig_pub.publish(dig_vel);
+        halt_pub_front_left.publish(halt_vel);
+        halt_pub_back_left.publish(halt_vel);
+        halt_pub_back_right.publish(halt_vel);
     } else {
         drive_pub.publish(*cmd_vel);
     }
