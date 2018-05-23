a80ae418d44c481b1d2dfea311ab7b241f796104
Chandler Watson <watsonc@stanford.edu>
2018-05-23 03:32:11 -0700

    Add basic digging framework

--- src/drive.cpp
+++ src/drive.cpp
@@ -1,52 +1,36 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
+#include "geometry_msgs/Twist.h"
+
+ros::Publisher drive_pub;
+bool digging_state = false;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg) {
-  std::cout << joy_msg->axes[0] << std::endl;
+    digging_state = joy_msg->buttons[4];
 }
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
+void twistCallback(const geometry_msgs::Twist::ConstPtr& cmd_vel) {
+    if (digging_state) {
+        // pass
+    } else {
+        drive_pub.publish(*cmd_vel);
+    }
+}
+
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "drive_controller");
-  ros::NodeHandle n;
+    ros::init(argc, argv, "drive_controller");
+    ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Subscriber joy_sub = n.subscribe<sensor_msgs::Joy>("joy", 1000, joyCallback);
+    ros::Subscriber joy_sub = n.subscribe<sensor_msgs::Joy>("joy", 1000, joyCallback);
+    ros::Subscriber twist_sub = n.subscribe<geometry_msgs::Twist>("cmd_vel", 1000, twistCallback);
+    drive_pub = n.advertise<geometry_msgs::Twist>("/rover_drive_controller/cmd_vel", 1000);
 
-  ros::Rate loop_rate(10);
-
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
-  while (ros::ok())
-    {
-      ros::spinOnce();
-
-      loop_rate.sleep();
-      ++count;
+    ros::Rate loop_rate(10);
+    while (ros::ok()) {
+        ros::spinOnce();
+        loop_rate.sleep();
     }
 
-
-  return 0;
+    return 0;
 }
